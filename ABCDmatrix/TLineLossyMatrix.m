5d6a6854768465adb4ba1acb6dfecb791362bda3
mk <mk@email.com>
2023-03-31 22:46:13 -0700

    Lossy tline

---
+++ ABCDmatrix/TLineLossyMatrix.m
@@ -0,0 +1,12 @@
+
+% R = series resistance
+% G = shunt conductance
+
+function M = TLineLossyMatrix(Z, Erad, R, G)
+    M = zeros(2)
+    M(1,1) = cos(Erad)
+    M(1,2) = R + sin(Erad) * Z * j
+    M(2,1) = G + sin(Erad) * Admittance(Z) * j
+    M(2,2) = cos(Erad)
+end
+
