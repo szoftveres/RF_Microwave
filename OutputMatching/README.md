a507e2171ad991fbd7c41365d0d25616f3077eb7
mk <mk@email.com>
2023-05-03 20:54:31 -0700

    Output matching

---
+++ OutputMatching/README.md
@@ -0,0 +1,14 @@
+# Output Matching Network
+
+Simulation of a loaded transmission line impedance transformer, peaked at 1GHz.
+
+The capacitively loaded 1/4 transmission line (due to change in boundary conditions) becomes resonant at a lower frequency - this can be advantageous, e.g. to create a narrow band impedance matching circuit, using the transmission line as the inductor.
+
+The unequal ratio of the loading capacitances ensures that the load (50 ohm) gets transformed to a higher impedance, e.g. to match the high impedance of the output of a transistor amplifier.
+
+![image circ](circ.png)
+
+![image plot](plot.png)
+
+
+
---
+++ OutputMatching/outputmatching.m
@@ -0,0 +1,53 @@
+% script matrix1
+
+% 900MHz - 1.1GHz
+sweeppoints = 800e+6:5e+6:1200e+6;
+
+% port impedance
+Z0 = 50 + 0j
+
+
+Z11realplot = []
+Z11imagplot = []
+
+% common functions
+addpath("../ABCDmatrix")
+
+for fp = 1:length(sweeppoints)
+    f = sweeppoints(fp)
+
+    % Parallel cap     
+    C1 = ParallelImpedanceMatrix(CapacitorImpedance(1.5e-12, f))
+
+    % TLine
+    T2 = TLineMatrix(100.0, f2rad(f, 1500e+6)/4)
+         
+    % Parallel cap
+    C3 = ParallelImpedanceMatrix(CapacitorImpedance(5e-12, f))
+
+    % Termination
+    R4 = ParallelImpedanceMatrix(Z0)
+
+    M = C1 * T2 * C3 * R4
+
+    %Z11 = A/C
+    Z = abcd2z(M)
+
+    % Z1,1 in ohms
+    Z11realplot = [Z11realplot; real(Z(1,1))]
+    Z11imagplot = [Z11imagplot; imag(Z(1,1))]
+
+end
+
+subplot(2, 2, 1)
+plot(sweeppoints, Z11realplot)
+xlabel("f(Hz)");
+ylabel("Z real (ohm)");
+
+subplot(2, 2, 2)
+plot(sweeppoints, Z11imagplot)
+xlabel("f(Hz)");
+ylabel("Z imag (ohm)");
+
+
+pause()
