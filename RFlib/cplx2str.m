42add16909f2ce65eacbc84a6821814d8a7a74cd
szoftveres <szoftveres@email.com>
2023-08-07 19:53:40 -0700

    Added labeling functions

---
+++ RFlib/cplx2str.m
@@ -0,0 +1,10 @@
+function str = cplx2str(cp)
+    cpi = imag(cp)
+    signs = "+"
+    if cpi < 0
+        cpi = abs(cpi)
+        signs = "-"
+    end
+    str = sprintf("%.2f%sj%.2f", real(cp), signs, cpi)
+end 
+
---
+++ RFlib/freq2str.m
@@ -0,0 +1,10 @@
+function str = freq2str(f)
+    fms =' kMGT'
+    fmsp = 1
+    while (f / 1000.0) > 1.0
+        f = f / 1000.0
+        fmsp = fmsp + 1
+    end
+    str = sprintf("%.2f %sHz", f, fms(fmsp))
+end
+
