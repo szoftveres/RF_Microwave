9b3743fbae6ca204e79e50a3d33a261749c7de30
szoftveres <szoftveres@email.com>
2025-03-15 18:28:37 -0700

    radar

---
+++ radar/README.md
@@ -0,0 +1,37 @@
+## Prototyping an FMCW Radar
+
+### Architecture
+
+The radar operates in the 902-928 MHz frequency band and uses mostly DIY components (including the antennas). The analog signals (IF and sweep sync) are fed into a stereo audio line-in interface of a PC and is recorded and saved as 48kHz sample rate stereo WAV file. The WAV file is then processed by the [processing script](https://github.com/szoftveres/RF_Microwave/tree/main/radar/fmcw_process.m).
+
+
+The sweep periodicity is set to approximately 100 Hz, with the sweep span being roughly 30 MHz. This gives a 3 GHz/sec chirp steepness. With c (speed of light) being approximately 300000 km/sec, the different target distances and associated IF frequencies can easily be calulated:
+
+15m distance -> 30m roudtrip -> 100ns time of fligt -> 300Hz IF
+150m distance -> 300m roudtrip -> 1us time of flight -> 3kHz IF
+
+Since we're only detectig the magnitude of the IF frequencies (ignoring the phase) at approximately 100Hz steps, the theoretical resolution is 5m. 
+The audio interface can reliably record frequencies up to at least ~15kHz, which gives a theoretical range of ~750 m (~ 1/2 mile).
+
+### Components
+
+#### VCO
+
+The VCO is a Colpitts type varicap-tuned dsicrete transistor oscillator with buffered output, designed to operate between roughly 800MHz and 1GHz. Its output level is relatively flat across the band, at +2dBm.
+
+![VCO_schem](VCO_schem.png)
+
+#### Ramp waveform generator
+
+The ramp generator is a Miller-integrator & Scmitt-trigger based circuit that produces a linear sawtooth waveform, as well as a sync signal that is used by the processing algorithm to detect the sweep starts and ends.
+
+![ramp_gen_schem](ramp_gen_schem.png)
+
+#### Analog frontend
+
+Since received RF signal experiences spherical expansion (Friis path loss) twice during its time in flight (once when transmitted by the transmitter antenna, and once when reflected back from a small, point-like object), the reflected signal strength of an object that was repositioned at double distance is only 1/16 of what the radar would receive when the object was at 1/2 distance. This has to be compensated for.
+Luckily, since increasing distance translates directly to increasing IF frequency, a compensation can easily be implemented in the analog frontend, in the form of a 2nd order (40 dB/decade) high-pass filter.
+
+![analog_fronted_schem](analog_fronted_schem.png)
+
+
---
+++ radar/fmcw_process.m
@@ -0,0 +1,151 @@
+
+pkg load signal;
+
+sync_channel = 2;
+sample_channel = 1;
+
+
+printf("\n\n == Reading data ..\n\n");
+
+[audiofile, samplerate] = audioread("radartest_car.wav");
+
+
+samplerate
+
+totalsamples = size(audiofile)(1)
+recording_length_seconds = totalsamples / samplerate
+
+
+% Separating the two (sync and sample) channels
+sync = [];
+sample = [];
+
+sync = rot90(audiofile(:, sync_channel));
+sample = rot90(audiofile(:, sample_channel));
+
+
+printf("\n\n == Detecting sweeps ..\n\n");
+
+sweeps = 0;
+sweepstarts = [];
+sweepends = [];
+in_sweep = 0;
+
+
+for i=2:totalsamples
+    if (sync(i-1) < 0.12 && sync(i) > 0.12 && not(in_sweep))
+        sweepstarts = [sweepstarts i];
+        in_sweep = 1;
+    endif
+    if (sync(i-1) > -0.12 && sync(i) < -0.12 && in_sweep)
+        sweepends = [sweepends i];
+        sweeps = sweeps + 1;
+        in_sweep = 0;
+    endif
+end
+
+sweeps
+sweeps_to_skip = 3
+periodicity_of_sweeps_hz = sweeps / recording_length_seconds
+
+
+sweepstarts = sweepstarts(sweeps_to_skip:sweeps-sweeps_to_skip);
+sweepends = sweepends(sweeps_to_skip:sweeps-sweeps_to_skip);
+
+
+% Calculating the number of samples within a sweep
+sweeps_counter = 0;
+lowest_samples_per_sweep = 9e+9;
+highest_samples_per_sweep = 0;
+average_samples_per_sweep = 0;
+sweeps_counter = length(sweepstarts);
+for i=1:sweeps_counter
+    samples_per_sweep = sweepends(i) - sweepstarts(i);
+    average_samples_per_sweep = average_samples_per_sweep + samples_per_sweep;
+    lowest_samples_per_sweep = min(lowest_samples_per_sweep, samples_per_sweep);
+    highest_samples_per_sweep = max(highest_samples_per_sweep, samples_per_sweep);
+end
+
+
+sweeps_counter
+lowest_samples_per_sweep
+average_samples_per_sweep = average_samples_per_sweep / sweeps_counter
+highest_samples_per_sweep
+
+printf("\n\n == Processing ..\n\n");
+
+samples_to_skip_front = round(lowest_samples_per_sweep / 6)
+samples_to_skip_back = round(lowest_samples_per_sweep / 20)
+
+
+subarray_size = 1+lowest_samples_per_sweep-samples_to_skip_back-samples_to_skip_front
+
+
+fft_base_Hz = samplerate / subarray_size
+
+% Processing
+avg_subarray = zeros(1,subarray_size);
+%window = ones(1,subarray_size);
+window = rot90(hanning(subarray_size));
+
+dataarray = [];
+subarray = [];
+for i=1:sweeps_counter
+    subarray = sample(sweepstarts(i)+samples_to_skip_front:sweepstarts(i)+lowest_samples_per_sweep-samples_to_skip_back);
+    subarray = abs(fft(subarray .* window));
+    avg_subarray = avg_subarray + subarray;
+
+    dataarray = [dataarray ; subarray];
+end
+
+avg_subarray = avg_subarray / sweeps_counter;
+
+
+% Removing averaged background return
+plotarray_timeslots = size(dataarray)(1);
+plotarray = [];
+for i=1:plotarray_timeslots
+    subarray = dataarray(i,:);
+    subarray = subarray - avg_subarray + 100;
+    subarray = log10(subarray(1:subarray_size/6));
+    plotarray = [plotarray ; subarray];
+end
+
+
+%plotarray_size = size(plotarray)
+
+
+printf("\n\n == Plotting ..\n\n");
+
+plotmin = min(min(plotarray));
+plotmax = max(max(plotarray));
+plotampl = (plotmax - plotmin);
+
+plotmean = mean(mean(plotarray));
+
+figure;
+%colormap ("gray");
+h = pcolor(flip(rot90(plotarray)));
+set(h, 'EdgeColor', 'none');
+caxis([(plotmean )      (plotmax - (plotampl * 0.20 ))]);
+
+
+sweepspan_Hz = 30e6
+c_meterpersec = 300e6
+sweeptime_sec = average_samples_per_sweep / samplerate
+sweepmagnitude_Hz_per_sec = sweepspan_Hz / sweeptime_sec
+
+for i=10:10:100
+    rack_freq = i * fft_base_Hz;
+    time_of_flight_sec = rack_freq / sweepmagnitude_Hz_per_sec;
+    roundtrip_meter = c_meterpersec * time_of_flight_sec;
+    target_meter = roundtrip_meter/2;
+    printf("    Y axis: %i  -   distance: %i m   -   echo %i Hz\n", i, target_meter, rack_freq);
+    
+end 
+
+
+printf("\nDone.\n");
+
+pause();
+
