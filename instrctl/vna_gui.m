c39155dfe76efad8e772b8cb96dafc607039c352
mk <szoftveres@users.noreply.github.com>
2026-05-16 09:54:47 -0700

    VNA calkit

--- instrctl/vna_gui.m
+++ instrctl/vna_gui.m
@@ -96,6 +96,72 @@ function mkr = markercalc(marker_khz, sweep)
     end
 end
 
+%
+% This function is called after a frequency (start, stop or step) change and
+% re-calculates the sweep, and calkit parameters based on the current calkit
+%
+function [sweep, correction] = recalc_vna (config, Z0)
+    sweep = config.startkhz:config.step_khz:config.stopkhz;
+    correction.g_open = [];
+    correction.g_short = [];
+    correction.g_load = [];
+    for i = 1:length(sweep)
+        correction.g_open = [correction.g_open keysight_cal_open(Z0, (sweep(i)*1000),
+                                                                 config.calkit.open(1),
+                                                                 config.calkit.open(2),
+                                                                 config.calkit.open(3),
+                                                                 config.calkit.open(4),
+                                                                 config.calkit.open(5),
+                                                                 config.calkit.open(6),
+                                                                 config.calkit.open(7))];
+        correction.g_short = [correction.g_short keysight_cal_short(Z0, (sweep(i)*1000),
+                                                                    config.calkit.short(1),
+                                                                    config.calkit.short(2),
+                                                                    config.calkit.short(3),
+                                                                    config.calkit.short(4),
+                                                                    config.calkit.short(5),
+                                                                    config.calkit.short(6),
+                                                                    config.calkit.short(7))];
+        correction.g_load = [correction.g_load keysight_cal_load(Z0, (sweep(i)*1000),
+                                                                    config.calkit.load(1),
+                                                                    config.calkit.load(2),
+                                                                    config.calkit.load(3),
+                                                                    config.calkit.load(4))];
+    end
+end
+
+
+%
+% This function is called after a frequency (start, stop or step) change or at startup
+% and invalidates + resets the calibration
+%
+function config = reset_cal (config, sweep)
+    config.s_open = ones(length(sweep));
+    config.s_short = ones(length(sweep)) * -1;
+    config.s_load = zeros(length(sweep));
+    config.s_iso = zeros(length(sweep));
+    config.s_thru = ones(length(sweep));
+    config.calstate = "-----";
+end
+
+
+%
+% This function is called on startup and initializes the calkit
+% with "ideal" parameters (HP / Keysight style)
+%
+function config = ideal_calkit (config)
+    %                       delay       loss   z0
+    %                       [s]         [w/s] [w]
+    % open                                            c0,c1,c2,c3
+    % short                                           l0,l1,l2,l3
+    % load                                            r
+    config.calkit.open =    [0,     0,     50,    1e-15, 0, 0, 0];
+    config.calkit.short =   [0,     0,     50,    1e-12, 0, 0, 0];
+    config.calkit.load =    [0,     0,     50,    50];
+    config.calkit.thru =    [0,     0,     50];
+end
+
+
 % =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =  =
 
 %serialportlist("available")
@@ -108,30 +174,13 @@ config.startkhz = 100000;
 config.stopkhz =  5800000;
 config.step_khz = 100000;
 
-sweep = config.startkhz:config.step_khz:config.stopkhz;
-config.mkr = floor(length(sweep)/2);
-
 config.level = -15;
 powerlevelchange(sp, config.level);
 
-config.s_open = ones(length(sweep));
-config.s_short = ones(length(sweep)) * -1;
-config.s_load = zeros(length(sweep));
-config.s_iso = zeros(length(sweep));
-config.s_thru = ones(length(sweep));
-
-
-
-
-g_open = [];
-g_short = [];
-g_load = [];
-for i = 1:length(sweep)
-    g_open = [g_open 1];
-    g_short = [g_short -1];
-    g_load = [g_load 0];
-end
-
+config = ideal_calkit(config);
+[sweep, correction] = recalc_vna (config, Z0);
+config = reset_cal(config, sweep);
+config.mkr = floor(length(sweep)/2);
 
 pause on;
 
@@ -199,24 +248,10 @@ function cb_powerchange (obj, init = false)
     set(fig, "userdata", action);
 end
 
-function cb_startchange (obj, init = false)
+function cb_freqchange (obj, init = false)
     fig = ancestor(obj,"figure","toplevel");
     action = get(fig, "userdata");
-    action.startchange = 2;
-    set(fig, "userdata", action);
-end
-
-function cb_stopchange (obj, init = false)
-    fig = ancestor(obj,"figure","toplevel");
-    action = get(fig, "userdata");
-    action.stopchange = 2;
-    set(fig, "userdata", action);
-end
-
-function cb_stepchange (obj, init = false)
-    fig = ancestor(obj,"figure","toplevel");
-    action = get(fig, "userdata");
-    action.stepchange = 2;
+    action.freqchange = 2;
     set(fig, "userdata", action);
 end
 
@@ -230,45 +265,45 @@ end
 fig = figure(1,"position",get(0,"screensize"));
 set (gcf, "color", get(0, "defaultuicontrolbackgroundcolor"));
 
-
+posh = 0.85;
 posv = 0.95;
 uicontrol ("style", "text",
            "units", "normalized",
            "string", "Calibrate",
            "horizontalalignment", "left",
-           "position", [0.85 posv 0.12 0.04]); posv -= 0.05;
+           "position", [posh posv 0.12 0.04]); posv -= 0.05;
 
 uicontrol ("style", "pushbutton",
            "units", "normalized",
            "callback", @cb_open_cal,
            "string", "OPEN",
-           "position", [0.85 posv 0.12 0.04]); posv -= 0.05;
+           "position", [posh posv 0.07 0.04]); posv -= 0.05;
 uicontrol ("style", "pushbutton",
            "units", "normalized",
            "callback", @cb_short_cal,
            "string", "SHORT",
-           "position", [0.85 posv 0.12 0.04]); posv -= 0.05;
+           "position", [posh posv 0.07 0.04]); posv -= 0.05;
 uicontrol ("style", "pushbutton",
            "units", "normalized",
            "callback", @cb_load_cal,
            "string", "LOAD",
-           "position", [0.85 posv 0.12 0.04]); posv -= 0.05;
+           "position", [posh posv 0.07 0.04]); posv -= 0.05;
 uicontrol ("style", "pushbutton",
            "units", "normalized",
            "callback", @cb_iso_cal,
            "string", "ISOL",
-           "position", [0.85 posv 0.12 0.04]); posv -= 0.05;
+           "position", [posh posv 0.07 0.04]); posv -= 0.05;
 uicontrol ("style", "pushbutton",
            "units", "normalized",
            "callback", @cb_thru_cal,
            "string", "THRU",
-           "position", [0.85 posv 0.12 0.04]); posv -= 0.05;
+           "position", [0.85 posv 0.07 0.04]); posv -= 0.05;
 posv -= 0.05;
 uicontrol ("style", "pushbutton",
            "units", "normalized",
            "callback", @cb_save_touchstone,
-           "string", "Save .s2p",
-           "position", [0.85 posv 0.12 0.04]); posv -= 0.05;
+           "string", "Export .s2p",
+           "position", [posh posv 0.12 0.04]); posv -= 0.05;
 
 posh = 0.7;
 posv = 0.95;
@@ -279,18 +314,8 @@ uicontrol ("style", "text",
            "position", [posh posv 0.12 0.04]); posv -= 0.05;
 uicontrol ("style", "pushbutton",
            "units", "normalized",
-           "callback", @cb_startchange,
-           "string", "Start",
-           "position", [posh posv 0.12 0.04]); posv -= 0.05;
-uicontrol ("style", "pushbutton",
-           "units", "normalized",
-           "callback", @cb_stopchange,
-           "string", "Stop",
-           "position", [posh posv 0.12 0.04]); posv -= 0.05;
-uicontrol ("style", "pushbutton",
-           "units", "normalized",
-           "callback", @cb_stepchange,
-           "string", "Step",
+           "callback", @cb_freqchange,
+           "string", "Freqency",
            "position", [posh posv 0.12 0.04]); posv -= 0.05;
 uicontrol ("style", "pushbutton",
            "units", "normalized",
@@ -323,7 +348,7 @@ uicontrol ("style", "pushbutton",
 
 
 
-posv = 0.25;
+posv = 0.4;
 conftext = uicontrol ("style", "text",
                       "units", "normalized",
                       "string", "Config:",
@@ -361,16 +386,14 @@ while true
     action.load_conf = 0;
     action.save_conf = 0;
     action.powerchange = 0;
-    action.startchange = 0;
-    action.stopchange = 0;
-    action.stepchange = 0;
+    action.freqchange = 0;
     action.markerchange = 0;
 
     set(fig, "userdata", action);
     set(msgbox, "string", action.msg);
-    set(conftext, "string", sprintf("Start: %i kHz\nStop: %i kHz\nStep: %i kHz\nPoints :%i\nLevel: %i dBm\nMarker: %i kHz\n",
+    set(conftext, "string", sprintf("Start: %i kHz\nStop: %i kHz\nStep: %i kHz\nPoints :%i\nLevel: %i dBm\nMarker: %i kHz\nCal: %s\n",
                                     config.startkhz, config.stopkhz, config.step_khz, length(sweep),
-                                    config.level, sweep(config.mkr)));
+                                    config.level, sweep(config.mkr), config.calstate));
 
     %tic()
     s_11 = sweep_freq_meas_refl (sp, sweep);
@@ -386,9 +409,9 @@ while true
         Scorr(1,1) = p1cal(S11, config.s_open(i),
                                 config.s_short(i),
                                 config.s_load(i),
-                                g_open(i),
-                                g_short(i),
-                                g_load(i));
+                                correction.g_open(i),
+                                correction.g_short(i),
+                                correction.g_load(i));
 
         % S2,1 error correction
         S21 = s_21(i);
@@ -396,8 +419,8 @@ while true
         Si = config.s_iso(i);
         Scorr(2,1) = conj((S21 - Si) / (St - Si));
 
-        Scorr(1,2) = Scorr(2,1); % This makes s2abcd and abcd2s functions happy
-        Scorr(2,2) = Scorr(1,1);
+        Scorr(1,2) = 1; % Scorr(2,1); % This makes s2abcd and abcd2s functions happy
+        Scorr(2,2) = 0; % Scorr(1,1);
         ts.points(i).S = Scorr;
     end 
 
@@ -408,56 +431,78 @@ while true
     action = get(fig, "userdata");
 
     if (action.save_touchstone > 0)
-        filename = uiputfile("*.s?p");
-        touchstonewrite(filename, ts);
-        action.msg = [filename " saved"];
+        filename = uiputfile({"*.s2p"});
+        if (filename)
+            touchstonewrite(filename, ts);
+            action.msg = [filename " saved"];
+        end
+    elseif (action.freqchange > 0)
+        ans = inputdlg({'Start (kHz)', 'Stop (kHz)', 'Step (kHz)'}, 'Frequency', 1, {config.startkhz, config.stopkhz, config.step_khz});
+        if (!isempty(ans))
+            startkhz = str2num(ans{1});
+            config.startkhz = startkhz;
+            stopkhz = str2num(ans{2});
+            config.stopkhz = stopkhz;
+            step_khz = str2num(ans{3});
+            config.step_khz = step_khz;
+            [sweep, correction] = recalc_vna (config, Z0);
+            config = reset_cal(config, sweep);
+            config.mkr = floor(length(sweep)/2);
+            action.msg = sprintf("Cal reset");
+        end
     elseif (action.powerchange > 0)
-        ans = inputdlg("Enter RF level (-30 dBm - 0 dBm)" );
-        lev = str2num(ans{1});
-        config.level = lev;
-        powerlevelchange(sp, config.level);
-        action.msg = sprintf("Level: %i dBm", config.level);
+        ans = inputdlg({"Enter RF level (-30 dBm - 0 dBm)"}, "RF level", 1, {config.level});
+        if (!isempty(ans))
+            lev = str2num(ans{1});
+            config.level = lev;
+            powerlevelchange(sp, config.level);
+            action.msg = sprintf("Level: %i dBm", config.level);
+        end
     elseif (action.markerchange > 0)
-        ans = inputdlg("Enter new marker (kHz)" );
-        khz = str2num(ans{1});
-        if ((khz < sweep(1)) || (khz > sweep(length(sweep))))
-            action.msg = "ERROR: marker out of range";
-        else
-            config.mkr = markercalc(khz, sweep); 
-            action.msg = sprintf("Marker: %i kHz", sweep(config.mkr));
+        ans = inputdlg({"Enter new marker (kHz)"} , "Marker", 1, {sweep(config.mkr)});
+        if (!isempty(ans))
+            khz = str2num(ans{1});
+            if ((khz < sweep(1)) || (khz > sweep(length(sweep))))
+                action.msg = "ERROR: marker out of range";
+            else
+                config.mkr = markercalc(khz, sweep); 
+                action.msg = sprintf("Marker: %i kHz", sweep(config.mkr));
+            end
         end
     elseif (action.opencal > 0)
         config.s_open = sweep_freq_meas_refl (sp, sweep);
+        config.calstate(1) = 'o';
         action.msg = "Open cal done";
     elseif (action.shortcal > 0)
         config.s_short = sweep_freq_meas_refl (sp, sweep);
+        config.calstate(2) = 's';
         action.msg = "Short cal done";
     elseif (action.loadcal > 0)
         config.s_load = sweep_freq_meas_refl (sp, sweep);
+        config.calstate(3) = 'l';
         action.msg = "Load cal done";
     elseif (action.isocal > 0)
         config.s_iso = sweep_freq_meas_thru (sp, sweep);
+        config.calstate(4) = 'i';
         action.msg = "Iso cal done";
     elseif (action.thrucal > 0)
         config.s_thru = sweep_freq_meas_thru (sp, sweep);
+        config.calstate(5) = 't';
         action.msg = "Thru cal done";
     elseif (action.save_conf > 0)
         filename = uiputfile("*.cfg");
-        save (sprintf ("%s", filename), "config")
-        action.msg = [filename " saved"];
-    elseif (action.load_conf > 0)
-        load(uigetfile("*.cfg"));
-        sweep = config.startkhz:config.step_khz:config.stopkhz;
-        g_open = [];
-        g_short = [];
-        g_load = [];
-        for i = 1:length(sweep)
-            g_open = [g_open 1];
-            g_short = [g_short -1];
-            g_load = [g_load 0];
+        if (filename)
+            save (sprintf ("%s", filename), "config")
+            action.msg = [filename " saved"];
         end
-        powerlevelchange(sp, config.level);
-        action.msg = "config recalled";
+    elseif (action.load_conf > 0)
+        filename = uigetfile("*.cfg");
+        if (filename)
+            load(filename);
+            [sweep, correction] = recalc_vna (config, Z0);
+            powerlevelchange(sp, config.level);
+            action.msg = "config recalled";
+        end
     end
 
 end
