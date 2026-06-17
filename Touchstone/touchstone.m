a105a97497beafb8d25f3e4c05ea21f0a992a36b
mk <szoftveres@users.noreply.github.com>
2026-06-16 17:24:07 -0700

    openems folder, touchstone

--- Touchstone/touchstone.m
+++ Touchstone/touchstone.m
@@ -1,17 +1,20 @@
 % script display touchstone
 
+graphics_toolkit qt
+
 % port impedance
 Z0 = 50;
 
 % common functions
 addpath("../RFlib")
 
-ts = touchstoneread('nanovna2.s2p');
-
-plot2ports(ts, 69)
-
-touchstonewrite('out.s2p', ts)
-
-pause()
+[filename, folder] = uigetfile();
+if (filename)
+    f = fullfile(folder, filename);
+    ts = touchstoneread(f);
+    plot2ports(ts, 69)
+    %touchstonewrite('out.s2p', ts)
+    pause()
+end
 
 
